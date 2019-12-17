08f53a8ec43a363628eeb33ed480ff2b39ba68b1
hemarajkumar <raj_hems@yahoo.com>
2019-12-17 19:35:22 +0000

    Update products.vue

--- src/modules/plp/products.vue
+++ src/modules/plp/products.vue
@@ -1,6 +1,64 @@
 <template>
-    <div>
-        <span class="product-list">{{ productTitle }}</span>
+    <div class="col-12 col-sm-12 noPad plp-products__list">
+        <div class="plp-products__product">
+            <div class="plp-products__img-container">
+                <a href="javascript:void(0);" class="js-navigatePdp plp-products__img-container--link">
+                    <img srcset="dist/images/clothing2.jpg" class="plp-products__img">
+                </a>
+            </div>
+            <span class="plp-products__name">{{ productTitle }}</span>
+            <div class="plp-products__price">
+                <span class="plp-products__now-price">
+                    £29.00
+                </span>
+            </div>
+            <button class="btn btn-danger plp-products--add-basket-btn">Add to Basket</button>
+		</div>
+
+         <div class="plp-products__product">
+            <div class="plp-products__img-container">
+                <a href="javascript:void(0);" class="js-navigatePdp plp-products__img-container--link">
+                    <img srcset="dist/images/clothing2.jpg" class="plp-products__img">
+                </a>
+            </div>
+            <span class="plp-products__name">{{ productTitle }}</span>
+            <div class="plp-products__price">
+                <span class="plp-products__now-price">
+                    £29.00
+                </span>
+            </div>
+            <button class="btn btn-danger plp-products--add-basket-btn">Add to Basket</button>
+		</div>
+
+         <div class="plp-products__product">
+            <div class="plp-products__img-container">
+                <a href="javascript:void(0);" class="js-navigatePdp plp-products__img-container--link">
+                    <img srcset="dist/images/clothing2.jpg" class="plp-products__img">
+                </a>
+            </div>
+            <span class="plp-products__name">{{ productTitle }}</span>
+            <div class="plp-products__price">
+                <span class="plp-products__now-price">
+                    £29.00
+                </span>
+            </div>
+            <button class="btn btn-danger plp-products--add-basket-btn">Add to Basket</button>
+		</div>
+
+         <div class="plp-products__product">
+            <div class="plp-products__img-container">
+                <a href="javascript:void(0);" class="js-navigatePdp plp-products__img-container--link">
+                    <img srcset="dist/images/clothing2.jpg" class="plp-products__img">
+                </a>
+            </div>
+            <span class="plp-products__name">{{ productTitle }}</span>
+            <div class="plp-products__price">
+                <span class="plp-products__now-price">
+                    £29.00
+                </span>
+            </div>
+            <button class="btn btn-danger plp-products--add-basket-btn">Add to Basket</button>
+		</div>
     </div>
 </template>
 
@@ -8,7 +66,7 @@
     export default {
         data () {
             return {
-                productTitle: 'List of products - will update soon'
+                productTitle: 'Mens Wear'
             }
         }
     }; 
