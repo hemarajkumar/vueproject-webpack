5d5fdea3ef71aacf2eb8e3dfdb7852dded91b473
hemarajkumar <raj_hems@yahoo.com>
2019-12-17 19:34:41 +0000

    Update facets.vue

--- src/modules/plp/facets.vue
+++ src/modules/plp/facets.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
-        <span class="facet-data">{{ facetTitle }}</span>
+        <div class="row">
+            <div class="plp-facets">
+                <div>
+                    <div class="col-12">
+                        <div class="plp-facets__types">
+                            <h3 class="plp-facets--title">{{ facetTitle }}</h3>
+                            <div class="plp-facets--option">
+                                <input id="11" type="checkbox" name="color" data-category="cat02" value="blue">
+                                <label for="11" class="plp-facets--option-label">blue</label>
+                                <br>
+                            </div>
+                            <div class="plp-facets--option">
+                                <input id="12" type="checkbox" name="color" data-category="cat02" value="red">
+                                <label for="12" class="plp-facets--option-label">red</label>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -8,7 +28,7 @@
     export default {
         data () {
             return {
-                facetTitle: 'Facet List - will update soon'
+                facetTitle: 'Mens Wear'
             }
         }
     }; 
